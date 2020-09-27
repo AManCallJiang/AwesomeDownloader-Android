8e6830c184a7e933e0ab31acd9aa294594e4f3cc
AManCallJiang <53471273+AManCallJiang@users.noreply.github.com>
2020-09-27 11:35:57 +0800

    PathSelector.kt 调整返回类型

--- AwesomeDownloader/src/main/java/com/jiang/awesomedownloader/tool/PathSelector.kt
+++ AwesomeDownloader/src/main/java/com/jiang/awesomedownloader/tool/PathSelector.kt
@@ -8,34 +8,58 @@ import android.os.Environment
  *
  * @ProjectName:    AwesomeDownloader
  * @ClassName:      PathSelector
- * @Description:     java类作用描述
+ * @Description:    路径选择工具
  * @Author:         江
  * @CreateDate:     2020/8/29 14:48
  */
 class PathSelector(private val context: Context) {
-    //   /data/user/0/{packageName}/cache
+
+    /**
+     * 返回手机内部储存的应用缓存路径 (/data/user/0/{packageName}/cache)
+     * @return String
+     */
     fun getCacheDirPath(): String = context.cacheDir.absolutePath
 
-    //    /storage/emulated/0/Android/data/{packageName}/cache
-    fun getExternalCacheDirPath(): String? = context.externalCacheDir?.absolutePath
+    /**
+     * 返回手机外部储存的应用缓存路径 (/storage/emulated/0/Android/data/{packageName}/cache)
+     * @return String
+     */
+    fun getExternalCacheDirPath(): String = context.externalCacheDir?.absolutePath!!
 
-    //    /storage/emulated/0/Android/data/{packageName}/files/Pictures
-    fun getPicturesDirPath(): String? =
-        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath
 
-    //    /storage/emulated/0/Android/data/{packageName}/files/Movies
-    fun getVideosDirPath(): String? =
-        context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.absolutePath
+    /**
+     * 返回手机外部储存的应用图片路径 (/storage/emulated/0/Android/data/{packageName}/files/Pictures)
+     * @return String
+     */
+    fun getPicturesDirPath(): String =
+        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath!!
 
-    //    /storage/emulated/0/Android/data/{packageName}/files/Music
-    fun getMusicDirPath(): String? =
-        context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath
+    /**
+     * 返回手机外部储存的应用影片路径 (/storage/emulated/0/Android/data/{packageName}/files/Movies)
+     * @return String
+     */
+    fun getVideosDirPath(): String =
+        context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.absolutePath!!
 
-    //    /storage/emulated/0/Android/data/{packageName}/files/Download
-    fun getDownloadsDirPath(): String? =
-        context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
+    /**
+     * 返回手机外部储存的应用音乐路径 (/storage/emulated/0/Android/data/{packageName}/files/Music)
+     * @return String
+     */
+    fun getMusicDirPath(): String =
+        context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath!!
+
+    /**
+     * 返回手机外部储存的应用下载文件路径 (/storage/emulated/0/Android/data/{packageName}/files/Download)
+     * @return String
+     */
+    fun getDownloadsDirPath(): String =
+        context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath!!
 
     //    /storage/emulated/0
-    fun getExternalRootDir(): String? = Environment.getExternalStorageDirectory().absolutePath
+    /**
+     * 返回手机外部储存的根目录 (/storage/emulated/0)
+     * @return String
+     */
+    fun getExternalRootDir(): String = Environment.getExternalStorageDirectory().absolutePath
 
 }
